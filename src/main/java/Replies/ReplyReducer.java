51b57d6cf1d3f210111aff9a2431a1fbb076c762
franciscodiaz11 <francisco.diaz11@upr.edu>
2018-10-13 14:45:47 -0400

    Added Reply Map Reduce

---
+++ src/main/java/Replies/ReplyReducer.java
@@ -0,0 +1,38 @@
+package Replies;
+
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+import java.io.IOException;
+public class ReplyReducer extends Reducer<Text, Text,Text,Text>{
+
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context)
+            throws IOException, InterruptedException {
+        //super.reduce(key, values, context);
+
+        // key is the abreviation of a state
+        // values is a list of 1s, one for each school found for the state
+
+        // setup a counter
+        String reply = "";
+        // iterator over list of 1s, to count them (no size() or length() method available)
+        for (Text value : values){
+            reply += value.toString()+" ";
+        }
+        // emit key-pair: key, count
+        // key is the abreviation for the state
+        // count is the number of schools in the state
+
+        //logger.trace("Red: " + key.toString());
+
+        // DEBUG
+        context.write(key, new Text(reply));
+    }
+}
+
+
